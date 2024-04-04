7d9ac023e6ab5476c1e252609268f59d0a1a715f
Ahmad <146891337+AhmadFA212@users.noreply.github.com>
2024-04-04 18:06:54 -0400

    Updated and added in a hook

--- react-test/src/Componenets/ChildComponent.jsx
+++ react-test/src/Componenets/ChildComponent.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import image from "/src/image.jpg";
 
-const ChildComponent = (Prop) => {
-  const img_link = "/src/image.jpg";
+const ChildComponent = (props) => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="child">
       <h2>This is the Child Component</h2>
-      <p>{Prop.message}</p>
+      <p>{props.message}</p>
+      <p>Count: {count}</p>
+      <button onClick={incrementCount}>Increment Count</button>
 
       <img src={image} style={{ width: "100px", height: "auto" }} alt="image" />
     </div>
